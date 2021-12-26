8be132d206503d3ceeb1aa3ab0efe857243990ac
tiannan <dengzheng2008@qq.com>
2021-12-26 18:04:13 +0800

    添加队列项目 删除持久化项目

---
+++ exercise/queue/queue.cpp
@@ -0,0 +1,181 @@
+﻿#include<error.h>
+#include"queue.h"
+namespace dz
+{
+	//ID
+	std::atomic<size_t> ID::m_GlobalNumber = 0;
+
+	ID::ID( )
+		:m_number(m_GlobalNumber.fetch_add(1))
+	{
+	}
+
+	ID::~ID( )
+	{
+	}
+
+	size_t ID::getID( )
+	{
+		return m_number;
+	}
+
+	void ID::reset( ) noexcept
+	{
+		m_number = m_GlobalNumber.fetch_add(1);
+	}
+
+	std::weak_ordering ID::operator<=>(const ID &right) const noexcept
+	{
+		return m_number<=> right.m_number;
+	}
+
+	//LumpNode
+
+	/*
+	* @brief 队列的大小（包含缓冲空间）
+	*/
+	const size_t LoopSize = 0x100;
+
+	const size_t LoopSize_ = 0xff;
+
+	template<typename T>
+	LumpNode<T>::LumpNode( )
+	{
+		m_loop.resize(LoopSize);
+		m_size = 0;
+		m_head = 0;
+		m_tail = 0;
+		m_next = nullptr;
+		m_number = m_GlobalNumber.get();
+	}
+
+	template<typename T>
+	LumpNode<T>::~LumpNode( )
+	{
+	}
+
+	template<typename T>
+	bool LumpNode<T>::push(T &&aim)
+	{
+		size_t index = m_tail.fetch_add(1);
+		if ((index - m_head.load( )) < LoopSize)
+		{
+			index &= 0xff;
+			m_loop[index] = aim;
+			m_size.fetch_add(1);
+			return ErrorCode::SUCCESS;
+		}
+		else
+		{
+			m_tail.fetch_sub(1);
+			return ErrorCode::FAIL;
+		}
+	}
+
+	template<typename T>
+	T LumpNode<T>::pop( )
+	{
+		auto LodSize = m_size.fetch_sub(1);
+		if (LodSize > 0)
+		{
+			T result;
+			size_t index = m_head.fetch_add(1);
+			index &= 0xff;
+			std::swap(m_loop[index] , result);
+			return result;
+		}
+		else
+		{
+			m_size.fetch_add(1);
+			return T( );
+		}
+	}
+
+	template<typename T>
+	int LumpNode<T>::size( )const noexcept
+	{
+		return m_size.load( );
+	}
+
+	template<typename T>
+	shared_ptr<LumpNode<T>> LumpNode<T>::getNext( ) const noexcept
+	{
+		return m_next;
+	}
+
+	template<typename T>
+	void LumpNode<T>::setNext(shared_ptr<LumpNode<T>> aim)noexcept
+	{
+		m_next = aim;
+	}
+
+	template<typename T>
+	void LumpNode<T>::Reset( ) noexcept
+	{
+		m_size = 0;
+		m_head = 0;
+		m_tail = 0;
+		m_next = nullptr;
+		ID::reset( );
+	}
+
+	//queue
+
+	template<typename T>
+	queue<T>::queue( )
+	{
+		m_size = 0;
+		m_headWork = make_shared<LumpNode<T>>();
+		m_tailWork = m_headWork;
+	}
+
+	template<typename T>
+	queue<T>::~queue( )
+	{
+	}
+
+	template<typename T>
+	void queue<T>::push(T &&aim)
+	{
+		bool err;
+		err = m_tailWork->push(std::forward<T>(aim));
+
+		m_size.fetch_add(1);
+	}
+
+	template<typename T>
+	T queue<T>::pop( )
+	{
+		int size = m_size.fetch_sub(1);
+		if (size > 0)
+		{
+			shared_ptr<LumpNode<T>> head = m_headWork;
+			T result = head->pop( ) , DefaultValue;
+			if (result == DefaultValue)
+			{
+				do
+				{
+					head = head->getNext( );
+					result = head->pop( );
+				} while (result == DefaultValue);
+				if (head->getID( ) > m_headWork->getID( ))
+				{
+					m_headWork.reset(head);
+				}
+			}
+				return result;
+		}
+		else
+		{
+			m_size.fetch_add(1);
+			return T( );
+		}
+	}
+
+	template<typename T>
+	int queue<T>::size( )const noexcept
+	{
+		return m_size;
+	}
+
+}
---
+++ exercise/queue/queue.h
@@ -0,0 +1,138 @@
+﻿#pragma once
+#include<def.h>
+#include<auto_ptr.h>
+
+#include<vector>
+#include<utility>
+namespace dz
+{
+	/*
+	* @brief 唯一ID。继承使用
+	*/
+	class ID
+	{
+	public:
+		ID( );
+		~ID( );
+		/*
+		* @brief 获取ID
+		* @return ID
+		*/
+		size_t getID( );
+
+		/*
+		* @brief 三路比较
+		*/
+		std::weak_ordering operator<=>(const ID &right)const noexcept;
+
+		/*
+		* @brief 重置，编号会自增
+		*/
+		void reset()noexcept;
+	private:
+		size_t m_number;
+		static std::atomic<size_t> m_GlobalNumber;
+	};
+
+	/*
+	* @brief 固定大小的队列
+	* @note 线程安全
+	*/
+	template<typename T>
+	class LumpNode:public ID
+	{
+	public:
+		LumpNode( );
+
+		/*
+		* @warning 不做任何安全保证，自己保证调用时已经没有竞争。
+		*/
+		~LumpNode( );
+
+		/*
+		* @brief 插入元素
+		* @param aim 待插入的值
+		* @return 成功与否
+		*/
+		bool push(T &&aim);
+
+		/*
+		* @brief 吐出元素
+		* @return 吐出的元素
+		* @warning 当空时，返回一个默认构造的元素
+		*/
+		T pop( );
+
+		/*
+		* @brief 返回当前队列大小（有效元素数量）
+		* @note 返回值可能小于零，但无关紧要
+		* @return 有效元素数量
+		*/
+		int size( )const noexcept;
+
+		/*
+		* @brief 返回下一个节点的指针
+		* @return 下一个节点的指针
+		*/
+		shared_ptr<LumpNode<T>> getNext( ) const noexcept;
+
+		/*
+		* @brief 返回下一个节点的指针
+		* @param aim 下一个节点的指针
+		* @return 无
+		*/
+		void setNext(shared_ptr<LumpNode<T>> aim) noexcept;
+
+		/*
+		* @brief 重置对象，也会修改对象的编号
+		* @return 无
+		*/
+		void Reset() noexcept;
+	private:
+		
+		std::vector<T> m_loop;
+		std::atomic<int> m_size;
+		std::atomic<int> m_head;
+		std::atomic<int> m_tail;
+		shared_ptr<LumpNode<T>> m_next;
+	};
+
+
+	/*
+	* @brief 浮动大小的队列
+	* @note 线程安全
+	*/
+	template<typename T>
+	class queue
+	{
+	public:
+		queue( );
+		~queue( );
+
+		/*
+		* @brief 插入元素
+		* @param aim 待插入的值
+		* @return 空
+		*/
+		void push(T &&aim);
+
+		/*
+		* @brief 吐出元素
+		* @return 吐出的元素
+		* @warning 当空时，返回一个默认构造的元素
+		*/
+		T pop( );
+
+		/*
+		* @brief 返回当前队列大小（有效元素数量）
+		* @return 有效元素数量
+		*/
+		int size( )const noexcept;
+	private:
+		std::atomic<int> m_size;//元素数量
+		shared_ptr<LumpNode<T>> m_headWork;//第一个节点
+		shared_ptr<LumpNode<T>> m_tailWork;//最后一个节点
+	};
+
+
+}
